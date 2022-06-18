928dd8d6eabc12c402228d96e670fe9f4cbbaba8
Aayush4580 <aayush.gtr619@gmail.com>
2022-06-18 20:57:25 +0530

    puah

---
+++ Heap/TopKFrequentElement.js
@@ -0,0 +1,18 @@
+var topKFrequent = function (nums, k) {
+  var hashMap = {};
+  for (var i = 0; i < nums.length; i++) {
+    if (!hashMap[nums[i]]) {
+      hashMap[nums[i]] = 1;
+    } else {
+      hashMap[nums[i]]++;
+    }
+  }
+  var keysSorted = Object.keys(hashMap).sort(function (a, b) {
+    return hashMap[b] - hashMap[a];
+  });
+  return keysSorted.slice(0, k);
+};
+var nums = [1, 2, 2, 3],
+  k = 2;
+
+console.log(topKFrequent(nums, k));
